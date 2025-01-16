e6e4fb8cdf671f990e8ed54f0db94ccb3fdc9097
SoonWooLee1 <150048350+SoonWooLee1@users.noreply.github.com>
2025-01-16 19:12:05 +0900

    Title: 체질량지수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250116/체질량지수/bmi.c
+++ 250116/체질량지수/bmi.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int h,w,b;
+    int h,w;
     scanf("%d %d", &h, &w);
-    b = (10000*w)/(h*h);
-    printf("%.0lf\n", b);
+    int b = (10000*w)/(h*h);
+    printf("%d\n", b);
     if(b>=25){
         printf("Obesity");
     }
