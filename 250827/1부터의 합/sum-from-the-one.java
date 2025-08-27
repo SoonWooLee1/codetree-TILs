f42b777c6747f4a80d7d77c6a6d71b16ad5d1987
SoonWooLee1 <150048350+SoonWooLee1@users.noreply.github.com>
2025-08-27 17:14:28 +0900

    Title: 1부터의 합, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250827/1부터의 합/sum-from-the-one.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int a = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            a += i;
+            if (a >= n) {
+                System.out.print(i);
+                break;
+            }
+        }
+    }
+}
